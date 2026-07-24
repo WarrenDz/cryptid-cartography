f2368f7a9c15a6fa976f2c814d0da192f873afe1
Warren Davison <49045641+WarrenDz@users.noreply.github.com>
2026-07-24 11:55:06 -0400

    remove unused image references

--- src/cryptids.js
+++ src/cryptids.js
@@ -1,5 +1,4 @@
 import bigfootImage from './assets/bigfoot.jpeg';
-import chupacabraImage from './assets/chupacabra.jpeg';
 
 const cryptids = {
     bigfoot: {
@@ -10,15 +9,6 @@ const cryptids = {
         hint2: 'Bigfoot-hint2',
         latitude: 47.6062,
         longitude: -122.3321
-    },
-    chupacabra: {
-        name: 'Chupacabra',
-        description: 'The Chupacabra is a legendary creature in the folklore of parts of the Americas, with its first purported sightings reported in Puerto Rico. The name comes from the animal\'s reported habit of attacking and drinking the blood of livestock, especially goats. The first reported sightings were in Puerto Rico, where it was described as a heavy creature, roughly the size of a small bear, with a row of spines reaching from the neck to the base of the tail.',
-        image: chupacabraImage,
-        hint1: 'Chupacabra-hint1',
-        hint2: 'Chupacabra-hint2',
-        latitude: 18.2208,
-        longitude: -66.5901
     }
 };
 
